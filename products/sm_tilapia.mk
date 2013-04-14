32789d16cb1a79281fa428d77307d8994c0e099a
Paul Beeler <sparksco@gmail.com>
2013-04-14 09:30:40 -0600

    Add tilapia Signed-off-by: Paul Beeler <sparksco@gmail.com>

---
+++ products/sm_tilapia.mk
@@ -0,0 +1,46 @@
+# Inherit AOSP device configuration for grouper.
+$(call inherit-product, device/asus/tilapia/full_tilapia.mk)
+
+# Inherit SaberMod common_tablet bits
+$(call inherit-product, vendor/sm/configs/common_tablet.mk)
+
+# Discard inherited values and use our own instead.
+PRODUCT_NAME := sm_tilapia
+PRODUCT_DEVICE := tilapia
+PRODUCT_BRAND := Google
+PRODUCT_MODEL := Nexus 7
+PRODUCT_MANUFACTURER := Asus
+
+PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=nakasig BUILD_FINGERPRINT="google/nakasig/tilapia:4.2.2/JDQ39/573038:user/release-keys" PRIVATE_BUILD_DESC="nakasig-user 4.2.2 JDQ39 573038 release-keys"
+
+# <--- Tilapia device depends on Grouper -->
+
+# Extra packages specific to Grouper
+PRODUCT_PACKAGES += \
+    GalleryGoogle-n7
+
+# Extra Required SaberMod packages
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver \
+    Stk
+
+# Boot animations
+PRODUCT_COPY_FILES += \
+    vendor/sm/prebuilts/grouper/system/media/bootanimation.zip:system/media/bootanimation.zip \
+    vendor/sm/prebuilts/grouper/system/media/bootanimations/1.zip:system/media/bootanimations/1.zip \
+    vendor/sm/prebuilts/grouper/system/etc/init.d/04bootanimations:system/etc/init.d/04bootanimations
+
+# remount scripts for system
+PRODUCT_COPY_FILES += \
+    vendor/sm/prebuilts/grouper/system/etc/mount-rw:system/etc/mount-rw \
+    vendor/sm/prebuilts/grouper/system/etc/mount-ro:system/etc/mount-ro
+
+ifeq ($(USING_BUILD_SCRIPTS),true)
+# kernel modules
+PRODUCT_COPY_FILES += \
+    vendor/sm/prebuilts/grouper/system/lib/modules/cifs.ko:system/lib/modules/cifs.ko
+endif
+
+# Copy the APN list over
+PRODUCT_COPY_FILES += \
+    vendor/sm/prebuilts/tilapia/system/etc/apns-conf.xml:system/etc/apns-conf.xml
