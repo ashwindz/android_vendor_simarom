e494458ca110ef0942587fe07feb84be6f767f23
Paul Beeler <sparksco@gmail.com>
2013-07-21 17:55:59 -0600

    Add SELinux filesystem relabeling to init
    Since not all recoveries out there will support the OTA packages'
    own labeling, check at boot if the system needs labels (and
    apply them)
    Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- configs/common.mk
+++ configs/common.mk
@@ -73,6 +73,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/sm/prebuilts/common/system/bin/sysinit:system/bin/sysinit
 
+# SELinux filesystem labels
+PRODUCT_COPY_FILES += \
+    vendor/sm/prebuilts/common/system/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
+
 # Busybox sync script
 PRODUCT_COPY_FILES += \
     vendor/sm/prebuilts/common/system/etc/init.d/99complete:system/etc/init.d/99complete
