4d12f17774092d805bc3116e2fe5a8cf16269abe
Paul Beeler <sparksco@gmail.com>
2013-07-22 12:12:29 -0600

    mako: build SEAndroidAdmin Signed-off-by: Paul Beeler <sparksco@gmail.com>

--- products/sm_mako.mk
+++ products/sm_mako.mk
@@ -25,6 +25,7 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occ
 # Extra packages specific to Mako
 PRODUCT_PACKAGES += \
     GalleryGoogle-mako \
+    SEAndroidAdmin \
     VideoEditorGoogle-mako \
     Torch
 
