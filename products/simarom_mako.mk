bbddd3d93b90770e9c7e96da23271b27c33be1c0
Martin Rodemann <martin@rodemann.eu>
2013-08-02 07:34:17 +0200

    fix derps in pathes

--- products/simarom_mako.mk
+++ products/simarom_mako.mk
@@ -1,15 +1,15 @@
 #GCC
-TARGET_GCC_VERSION_EXP := 4.9
+TARGET_GCC_VERSION_EXP := 4.7
 
 # Inherit SaberMod common gsm bits
-$(call inherit-product, vendor/sm/configs/gsm.mk)
+$(call inherit-product, vendor/simarom/configs/gsm.mk)
 
 # Inherit AOSP device configuration for grouper.
 $(call inherit-product, device/lge/mako/aosp_mako.mk)
 
 # Mako overlay
 PRODUCT_PACKAGE_OVERLAYS += \
-    vendor/sm/overlay/mako
+    vendor/simarom/overlay/mako
 
 PRODUCT_NAME := simarom_mako
 PRODUCT_DEVICE := mako
@@ -21,19 +21,19 @@ PRODUCT_BUILD_PROP_OVERRIDES += PRODUCT_NAME=occam BUILD_FINGERPRINT="google/occ
 
 # APN list
 PRODUCT_COPY_FILES += \
-    vendor/sm/prebuilts/mako/system/etc/apns-conf.xml:system/etc/apns-conf.xml
+    vendor/simarom/prebuilts/mako/system/etc/apns-conf.xml:system/etc/apns-conf.xml
 
 # Kernel modules
 ifeq ($(USING_BUILD_SCRIPTS),true)
 PRODUCT_COPY_FILES += \
-    vendor/sm/prebuilts/mako/system/lib/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
-    vendor/sm/prebuilts/mako/system/lib/modules/cifs.ko:system/lib/modules/cifs.ko
+    vendor/simarom/prebuilts/mako/system/lib/modules/scsi_wait_scan.ko:system/lib/modules/scsi_wait_scan.ko \
+    vendor/simarom/prebuilts/mako/system/lib/modules/cifs.ko:system/lib/modules/cifs.ko
 endif
 
 # # remount scripts for system
 PRODUCT_COPY_FILES += \
-    vendor/sm/prebuilts/mako/system/etc/mount-rw:system/etc/mount-rw \
-    vendor/sm/prebuilts/mako/system/etc/mount-ro:system/etc/mount-ro
+    vendor/simarom/prebuilts/mako/system/etc/mount-rw:system/etc/mount-rw \
+    vendor/simarom/prebuilts/mako/system/etc/mount-ro:system/etc/mount-ro
 
 # mako audio
 -include frameworks/base/data/sounds/AudioPackage10.mk
